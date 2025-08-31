3853d27f6469f3fbfc34f4b9396732a6dc687176
adel mohamed <adel3052003adel@gmail.com>
2025-08-31 14:15:59 +0300

    Initial commit - NavMaster project

---
+++ script.js
@@ -0,0 +1,7 @@
+// Toggle mobile menu
+const hamburger = document.getElementById("hamburger");
+const navLinks = document.getElementById("navLinks");
+
+hamburger.addEventListener("click", () => {
+  navLinks.classList.toggle("show");
+});
